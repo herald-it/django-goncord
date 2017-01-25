9a55936cae459c367243358d9904d3e3288f4386
lorens42rus <scroll42ru@gmail.com>
2017-01-25 15:31:20 +0700

    Returned validation per request

--- django_goncord/backends.py
+++ django_goncord/backends.py
@@ -1,69 +1,69 @@
-from django.contrib.auth import logout
-from django.contrib.auth import settings
+from django.contrib.auth.middleware import RemoteUserMiddleware
 from django.core.exceptions import ImproperlyConfigured
+from django.contrib import auth
 
-import requests
+from app.backends import goncord
+
+import base64
+import json
 
 
-class Goncord(object):
+class GoncordMiddleware(RemoteUserMiddleware):
 
-    login_url = ""
-    logout_url = ""
-    validate_url = ""
-    register_url = ""
-
-    def __init__(self):
-        if not hasattr(settings, 'GONCORD'):
-            raise ImproperlyConfigured('Register GONCORD in settings required')
-
-        if 'BASE_URL' not in settings.GONCORD:
-            raise ImproperlyConfigured(
-                'Register BASE_URL in settings.GONCORD required')
-
-        if 'LOGIN_URL' not in settings.GONCORD:
-            raise ImproperlyConfigured(
-                'Register LOGIN_URL in settings.GONCORD required')
-        self.login_url = '%s%s' % (
-            settings.GONCORD['BASE_URL'], settings.GONCORD['LOGIN_URL'])
-
-        if 'LOGOUT_URL' not in settings.GONCORD:
-            raise ImproperlyConfigured(
-                'Register LOGOUT_URL in settings.GONCORD required')
-        self.logout_url = '%s%s' % (
-            settings.GONCORD['BASE_URL'], settings.GONCORD['LOGOUT_URL'])
-
-        if 'VALIDATE_URL' not in settings.GONCORD:
-            raise ImproperlyConfigured(
-                'Register VALIDATE_URL in settings.GONCORD required')
-        self.validate_url = '%s%s' % (
-            settings.GONCORD['BASE_URL'], settings.GONCORD['VALIDATE_URL'])
-
-        if 'REGISTER_URL' not in settings.GONCORD:
-            raise ImproperlyConfigured(
-                'Register REGISTER_URL in settings.GONCORD required')
-        self.register_url = '%s%s' % (
-            settings.GONCORD['BASE_URL'], settings.GONCORD['REGISTER_URL'])
-
-    def login(self, request, data):
-        return requests.post(
-            self.login_url, data)
-
-    def logout(self, request):
-        logout(request)
-        requests.post(self.logout_url, cookies=request.COOKIES)
+    cookie_name = 'jwt'
 
     def validate(self, request):
-        return requests.post(self.validate_url,
-                             cookies=request.COOKIES)
+        r = goncord.validate(request)
 
-    def register(self, request, data):
-        return requests.post(self.register_url, data)
+        if r.status_code != 200:
+            return
 
-    def update_user(self, user, data):
-        user.first_name = data['payload']['first_name']
-        user.last_name = data['payload']['last_name']
-        user.email = data['email']
-        user.save()
+        creds = r.json()
+        if creds['payload']:
+            creds['payload'] = json.loads(creds['payload'].replace("'", '"'))
 
+        return creds
 
-goncord = Goncord()
+    def decode_base64(self, data):
+        missing_padding = len(data) % 4
+        if missing_padding != 0:
+            data += '=' * (4 - missing_padding)
+        return base64.b64decode(data).decode()
+
+    def process_request(self, request):
+
+        if not hasattr(request, 'user'):
+            raise ImproperlyConfigured(
+                "The Django remote user auth middleware requires the"
+                " authentication middleware to be installed.  Edit your"
+                " MIDDLEWARE setting to insert"
+                " 'django.contrib.auth.middleware.AuthenticationMiddleware'"
+                " before the RemoteUserMiddleware class.")
+
+        try:
+            token = request.COOKIES[self.cookie_name]
+        except KeyError:
+            if request.user.is_authenticated:
+                self._remove_invalid_user(request)
+            return
+
+        payloads = self.validate(request)
+        if payloads is None:
+            return
+
+        if request.user.is_authenticated():
+            payloads = token.split('.')[1]
+            payloads = json.loads(self.decode_base64(payloads))
+            if request.user.get_username() == \
+                    self.clean_username(payloads['login'], request):
+                return
+            else:
+                self._remove_invalid_user(request)
+
+        if not request.user.is_authenticated():
+            user = auth.authenticate(remote_user=payloads['login'])
+            goncord.update_user(user, payloads)
+
+        if user:
+            request.user = user
+            auth.login(request, user)
